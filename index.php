f3641d07452a0b5cf4dc31c28ea28138ba421232
Liibaan <liibaanlii86@gmail.com>
2019-03-04 17:17:35 +0100

    A few minor changes

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset ="UTF-8">
+    <title>Dit is voor pagina</title>
+<body> <h1> Welkom op mijn website</h1>
+
+<nav> <a href = "index.html"> Home</a> </nav>
+<main>
+    <!-- Hier komt niews artikel te staan-->
+    <section>
+        <header>
+            <h1>Bla bla bla</h1>
+            <h2> En nog bla bla</h2>
+        </header>
+
+        <p>Somige mensen praten te veel, elke keer zegen ze bla bla bla</p>
+        <div id = "inleiding"> Bla bla bla
+        </div>
+        <div id = "alinea"> En nog bla bla bla...
+        </div>
+        Vanaf morgen geven we <span id="korting">gigantische kortingen</span> op onze
+        extra gewatteerde zuurstofpakken! Aarzel niet en grijp je kans om ook je
+        ruimtewandeling te maken!
+        <span id="link"><a href="blablabala.html"> [Lees meer]</a>
+</span>
+    </section>
+    <article>
+        <p> Volgende week zondag vindt het 115de Interplanetairsongfestival plaats op Mars.
+            Met meer dan 40 deelnemende planeten.</p>
+        <aside>
+            <h3> Winnaars afgelopen 10 jaar</h3>
+            <ul>
+                <li>39201: K. Wazelzo (Marcurius)</li>
+                <li>39200:  Daeme (2001 DY59)</li>
+                <li>39199: Patshoe (Delta Aquilae A)</li>
+                <li>...</li>
+            </ul>
+        </aside>
+    </article>
+
+
+</main>
+<footer> <!-- hier komen copyrightinformatie en contactgegevens te staan-->
+</footer>
+
+</body>
+</html>
